b2d6b745e6e9667d294e9e21eeccaba5b5533f33
orut34iop <h64ut2qg5lou013@gmail.com>
2024-12-29 22:49:44 +0800

    删除主界面中的导出库文件选项卡

--- tabs/__init__.py
+++ tabs/__init__.py
@@ -2,7 +2,6 @@ from .export_symlink_tab import ExportSymlinkTab
 from .delete_symlink_tab import DeleteSymlinkTab
 from .check_duplicate_tab import CheckDuplicateTab
 from .merge_files_tab import MergeFilesTab
-from .export_library_tab import ExportLibraryTab
 from .merge_version_tab import MergeVersionTab
 from .update_category_tab import UpdateCategoryTab
 
@@ -11,7 +10,6 @@ __all__ = [
     'DeleteSymlinkTab',
     'CheckDuplicateTab',
     'MergeFilesTab',
-    'ExportLibraryTab',
     'MergeVersionTab',
     'UpdateCategoryTab'
 ]
