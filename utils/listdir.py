5e4d390b13b4e1278e2228a01f5b49f396ce3c0e
orut34iop <h64ut2qg5lou013@gmail.com>
2025-01-04 23:38:59 +0800

    解决合并文件list_files报异常的问题

--- utils/listdir.py
+++ utils/listdir.py
@@ -16,12 +16,17 @@ def generate_output_filename(folder_path: str) -> str:
     timestamp = time.strftime('%Y%m%d_%H%M%S')
     return f"{folder_name}_files_{timestamp}.txt"
 
-def list_files(folder_path: str, enable_115_protect,output_path = "",is_first_call = True,logger = None) -> tuple[int, list[str], str]:
+def list_files(folder_path: str, enable_115_protect: bool, op_interval_sec: int, output_path = "", is_first_call = True, logger = None) -> tuple[int, list[str], str]:
 
     list_files_logger = logger or logging.getLogger(__name__)  # 使用传递的logger
     """
     遍历文件夹，获取所有文件的绝对路径并保存到文本文件
     :param folder_path: 要遍历的文件夹路径
+    :param enable_115_protect: 是否启用115防封
+    :param op_interval_sec: 操作间隔时间（秒）
+    :param output_path: 输出文件路径
+    :param is_first_call: 是否是第一次调用
+    :param logger: 日志记录器
     :return: (文件数量, 文件路径列表, 输出文件路径)
     """
     if is_first_call:
@@ -38,13 +43,13 @@ def list_files(folder_path: str, enable_115_protect,output_path = "",is_first_ca
         # 删除同名文件
         if os.path.exists(output_path):
             try:
-                os.remove(file_path)
+                os.remove(output_path)
             except OSError as e:
-                list_files_logger.error(f"无法删除文件 {file_path}: {e}")
+                list_files_logger.error(f"无法删除文件 {output_path}: {e}")
     else:
         if enable_115_protect:
             list_files_logger.info("list_files 115防封已启用")
-            time.sleep(2)
+            time.sleep(int(op_interval_sec))
 
         list_files_logger.info("list_files : This is sub call.")
         list_files_logger.info(f"folder_path: {folder_path}")
@@ -63,13 +68,12 @@ def list_files(folder_path: str, enable_115_protect,output_path = "",is_first_ca
                 list_files_logger.debug(f"找到文件: {abs_path}")
             elif entry.is_dir():
                 # 递归调用list_files处理子目录
-                sub_folder_count, sub_folder_files, _ = list_files(entry.path,enable_115_protect,output_path,is_first_call=False, logger=list_files_logger)
+                sub_folder_count, sub_folder_files, _ = list_files(entry.path, enable_115_protect, op_interval_sec, is_first_call=False, logger=list_files_logger)
                 file_paths.extend(sub_folder_files)
 
         if not file_paths:
             list_files_logger.info(f"文件夹为空: {folder_path}")
             return 0, [], ""
-
 
         file_count = len(file_paths)
 
